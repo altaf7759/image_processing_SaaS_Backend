ed619de1540307618e064e7162f931eda20753a1
Altaf Raja <altaf.1040216@gmail.com>
2026-05-30 10:16:25 +0530

    cors for development

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 
 import "./workers/image.worker.js"
 import "./workers/email.worker.js"
@@ -14,6 +15,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+      cors({
+            origin: "http://localhost:5173",
+            credentials: true,
+      })
+);
 
 app.get("/health", (req, res) => {
       res.status(200).json({ status: "ok" });
