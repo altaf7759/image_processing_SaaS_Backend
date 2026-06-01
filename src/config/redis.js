f26a74db29601268e600c80e940d96fcbac9b4e4
Altaf Raja <altaf.1040216@gmail.com>
2026-06-01 18:55:52 +0530

    redis config for both local with docker and redis cloud implemented

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,13 +1,25 @@
 import IORedis from "ioredis";
 
-export const redisClient = new IORedis({
-      host: process.env.REDIS_HOST || "localhost",
-      port: Number(process.env.REDIS_PORT) || 6379,
+const redisConfig = process.env.REDIS_URL
+      ? {
+            host: process.env.REDIS_URL,
+            port: Number(process.env.REDIS_PORT_CLOUD),
+            username: process.env.REDIS_USERNAME || "default",
+            password: process.env.REDIS_PASSWORD,
+      }
+      : {
+            host: process.env.REDIS_HOST || "localhost",
+            port: Number(process.env.REDIS_PORT) || 6379,
+      };
+
+export const redisClient = new IORedis(redisConfig, {
       maxRetriesPerRequest: null,
       enableReadyCheck: false,
       retryStrategy(times) {
-            return Math.min(times * 500, 5000)
-      }
+            return Math.min(times * 500, 5000);
+      },
 });
 
-redisClient.on("error", (err) => console.error("Redis Error:", err));
+redisClient.on("error", (err) => {
+      console.error("Redis Error:", err);
+});
