14ff676ebb572c2e91b68249cfd21c943bc2d234
Altaf Raja <altaf.1040216@gmail.com>
2026-06-01 19:39:18 +0530

    postges with neon db setup

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,18 @@
 import Pool from 'pg';
 
-const pool = new Pool.Pool({
+const poolConfig = process.env.NODE_ENV === 'production' ? {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+            rejectUnauthorized: false
+      }
+} : {
       user: process.env.POSTGRES_USER,
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-})
+}
+
+const pool = new Pool.Pool(poolConfig);
 
 export default pool;
